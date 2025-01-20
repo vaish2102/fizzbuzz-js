bc0d12ba313b964b9a044fd5d65e6bec124c94d8
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-01-20 15:45:17 +0000

    printing Fizz for multiples of 3 part 3 of the task

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,7 +6,13 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
     console.log('Hello World');
     for(let i = 0 ; i<100;i++){
-        console.log(i+1);
+        if( (i+1) %3 == 0) {
+            console.log("Fizz");
+        }
+        else{
+            console.log(i+1);
+        }
+        
     }
 
     // Put your code here...
